1c94421b8a100a1b013916de564f320e09c66c30
jakeblackburnn <jackblackburnn@icloud.com>
2026-02-07 15:00:02 -0500

    Fix test experiment path

--- run_mc.py
+++ run_mc.py
@@ -1,9 +1,8 @@
 from monte_carlo.runner import MonteCarlo, MonteCarloConfig, Experiment
-from monte_carlo.experiments import TestExperiment
+from monte_carlo.experiments.test import TestExperiment
 
 config = MonteCarloConfig()
 runner = MonteCarlo(config)
 test_experiment = TestExperiment()
 
-# AAHAHAHAHHA
 runner.run(test_experiment)
