044318a6eb9b5eeeb68749472de19bfaedd0d189
jakeblackburnn <jackblackburnn@icloud.com>
2026-02-12 15:02:12 -0500

    Try different settings

--- run_mc.py
+++ run_mc.py
@@ -1,13 +1,12 @@
 from monte_carlo.runner import MonteCarlo, MonteCarloConfig, Experiment
 from monte_carlo.experiments.easy_seven import EasyExperiment, EasyResult
 
-config = MonteCarloConfig(num_simulations=10000)
+config = MonteCarloConfig(num_simulations=1000)
 result = EasyResult(config_dict=config.to_dict())
-exp    = EasyExperiment()
+exp    = EasyExperiment(inv_size=24)
 
 runner = MonteCarlo(config, result)
 
-# TODO: time this shit
 runner.run(exp)
 
 result.summary()
